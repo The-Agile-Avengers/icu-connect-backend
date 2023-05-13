4f83ba657162b21058ca49c44a3d549a06e901f3
solodezaldivar <ramon.solodezaldivar@uzh.ch>
2023-05-13 12:31:29 +0200

    feat: update localstack script

--- localstack-windows.ps1
+++ localstack-windows.ps1
@@ -1,17 +1,23 @@
-Write-Host "Listening for localstack startup"
 Write-Host "." -NoNewline
-awslocal ecs register-task-definition --cli-input-json file://./containerDefinitions.json
-$family = 'icubackend-task-definition'
-$containerDefinitions = '[ { "name": "icubackend", "image": "localhost.localstack.cloud:4511/icubackend:latest", "essential": true, "portMappings": [ { "containerPort": 8080, "protocol": "tcp" } ], "logConfiguration": { "logDriver": "awslogs", "options": { "awslogs-group": "<your-log-group>", "awslogs-region": "eu-west-1", "awslogs-stream-prefix": "icubackend" } } } ]'
+Write-Host "`n# stop and destroy existing containers"
+docker stop $(docker ps -aq) && docker rm $(docker ps -aq)
+Write-Host "Done"
+Start-Process powershell.exe -ArgumentList "docker run --rm -it -p 4566:4566 -p 4510-4559:4510-4559 -v /var/run/docker.sock:/var/run/docker.sock -e PERSISTENCE=1 -e LOCALSTACK_API_KEY=50r6qhtzfz localstack/localstack-pro" -WindowStyle Normal
 
+Start-Sleep -s 5
 while ((Test-NetConnection -ComputerName localhost -Port 4566 -InformationLevel Quiet) -ne $true)
 {
     Write-Host "." -NoNewline
+    Write-Host "Listening for localstack startup"
+    
     Start-Sleep -s 0.5
     Write-Host "Connected to localstack (localhost:4566)" -ForegroundColor Green
     
     Write-Host "Setup Localstack Env"
 }
+awslocal ecs register-task-definition --cli-input-json file://./containerDefinitions.json
+$family = 'icubackend-task-definition'
+$containerDefinitions = '[ { "name": "icubackend", "image": "localhost.localstack.cloud:4511/icubackend:latest", "essential": true, "portMappings": [ { "containerPort": 8080, "protocol": "tcp" } ], "logConfiguration": { "logDriver": "awslogs", "options": { "awslogs-group": "<your-log-group>", "awslogs-region": "eu-west-1", "awslogs-stream-prefix": "icubackend" } } } ]'
 Write-Host "Setup ECS"
 awslocal ecs create-cluster --cluster-name icubackend-cluster
 Write-Host "# Done:ECS Setup"
@@ -20,7 +26,7 @@ awslocal s3api create-bucket --bucket icufiles --create-bucket-configuration Loc
 Write-Host "# Setup file store S3 bucket"
 Write-Host "S3 bucket icufiles created" -ForegroundColor Green
 
-awslocal rds create-db-cluster --db-cluster-identifier dbcluster1 --engine mysql --database-name icudb1
+awslocal rds create-db-cluster --db-cluster-identifier dbcluster1 --engine mysql --database-name icudb1 --port 4510
 Write-Host "# setup RDS MySql database"
 Write-Host "RDS MySql database created" -ForegroundColor Green
 
@@ -29,8 +35,10 @@ Write-Host "# build maven project (JAR)"
 Write-Host "Maven project built successfully" -ForegroundColor Green
 
 awslocal ecr create-repository --repository-name icubackend
+Start-Sleep -s 1
+
 docker build -t localhost:4511/icubackend .
-docker push localhost:4511/icubackend:latest
+docker push localhost:4511/icubackend
 Write-Host "# register backend image in ECR"
 Write-Host "Backend image registered in ECR" -ForegroundColor Green
 
@@ -56,6 +64,7 @@ while (-not $port)
 {
     $port = docker ps --format "table {{.Names}}\t{{.Ports}}" | Select-String "8080/tcp" | ForEach-Object { $_.ToString().Split(' ')[-1].Split('-')[0] }
     if (-not $port) {
+        Write-Host "Looking for EC2 Port"
         Start-Sleep -Seconds 2
         }
 }
@@ -63,22 +72,12 @@ while (-not $port)
 Write-Host "The ECS container port is $port"
 
 
-Write-Host "██╗ ██████╗██╗   ██╗"
-Write-Host "██║██╔════╝██║   ██║"
-Write-Host "██║██║     ██║   ██║"
-Write-Host "██║██║     ██║   ██║"
-Write-Host "██║╚██████╗╚██████╔╝"
-Write-Host "╚═╝ ╚═════╝ ╚═════╝  Fachverein Informatik"
+Write-Host -ForegroundColor Green  "██╗ ██████╗██╗   ██╗" 
+Write-Host -ForegroundColor Green  "██║██╔════╝██║   ██║" 
+Write-Host -ForegroundColor Green  "██║██║     ██║   ██║" 
+Write-Host -ForegroundColor Green  "██║██║     ██║   ██║" 
+Write-Host -ForegroundColor Green  "██║╚██████╗╚██████╔╝" 
+Write-Host -ForegroundColor Green  "╚═╝ ╚═════╝ ╚═════╝  Fachverein Informatik" 
 
-Write-Host " ______                 ___                ______                                                            "
-Write-Host "/\  _  \            __ /\_ \              /\  _  \                                                           "
-Write-Host "\ \ \L\ \      __  /\_\\//\ \       __    \ \ \L\ \   __  __     __     ___       __       __   _ __   ____  "
-Write-Host " \ \  __ \   /'_ `\\/\ \ \ \ \    /'__`\   \ \  __ \ /\ \/\ \  /'__`\ /' _ `\   /'_ `\   /'__`\/\`'__\/',__\ "
-Write-Host "  \ \ \/\ \ /\ \L\ \\ \ \ \_\ \_ /\  __/    \ \ \/\ \\ \ \_/ |/\  __/ /\ \/\ \ /\ \L\ \ /\  __/\ \ \//\__, `\"
-Write-Host "   \ \_\ \_\\ \____ \\ \_\/\____\\ \____\    \ \_\ \_\\ \___/ \ \____\\ \_\ \_\\ \____ \\ \____\\ \_\\/\____/"
-Write-Host "    \/_/\/_/ \/___L\ \\/_/\/____/ \/____/     \/_/\/_/ \/__/   \/____/ \/_/\/_/ \/___L\ \\/____/ \/_/ \/___/ "
-Write-Host "               /\____/                                                            /\____/                    "
-Write-Host "               \_/__/                                                             \_/__/                     "
-Write-Host ""
 
 Write-Host "Environment is running. You can close this window"
